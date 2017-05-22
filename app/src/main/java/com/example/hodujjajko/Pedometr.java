a0c31b84c729080d9b461aae58bafb6e144fb3bb
Kasia <kasia.haja@gmail.com>
2017-05-22 21:21:18 +0200

    draft of pedometr

--- app/src/main/java/com/example/hodujjajko/Pedometr.java
+++ app/src/main/java/com/example/hodujjajko/Pedometr.java
@@ -2,41 +2,46 @@ package com.example.hodujjajko;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
-
-/**
- * Created by Kasia on 2017-05-21.
- */
 
 public class Pedometr extends Activity implements SensorEventListener {
     private TextView counter;
     private SensorManager sensorManager;
     private Sensor stepCounter;
     private Sensor stepDetector;
+    boolean activityRunning;
 
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.pedometr);
         counter = (TextView) findViewById(R.id.conterTextView);
+        Log.i("Pedometr","jestem w create");
+        PackageManager pm = getPackageManager();
+        if (pm.hasSystemFeature(PackageManager.FEATURE_SENSOR_STEP_COUNTER)) {
+            Log.i("Pedometr","czujnik jest widoczny");
+        }else{
+            Log.i("Pedometr","Brak czujnika");
+        }
 
-        sensorManager = (SensorManager)
-                getSystemService(Context.SENSOR_SERVICE);
-        stepCounter = sensorManager
-                .getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
-        stepDetector = sensorManager
-                .getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        stepCounter = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        stepDetector = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
     }
     @Override
     protected void onResume() {
 
         super.onResume();
+        activityRunning = true;
         if (stepCounter != null) {
             sensorManager.registerListener(this,stepCounter, SensorManager.SENSOR_DELAY_FASTEST);
             sensorManager.registerListener(this,stepDetector, SensorManager.SENSOR_DELAY_FASTEST);
@@ -48,23 +53,13 @@ public class Pedometr extends Activity implements SensorEventListener {
     @Override
     protected void onPause() {
         super.onPause();
+        activityRunning = false;
     }
 
     @Override
     public void onSensorChanged(SensorEvent event) {
-        Sensor sensor = event.sensor;
-        float[] values = event.values;
-        int value = -1;
-
-        if (values.length > 0) {
-            value = (int) values[0];
-        }
-
-        if (sensor.getType() == Sensor.TYPE_STEP_COUNTER) {
-            counter.setText("Step Counter Detected : " + value);
-        } else if (sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
-            // For test only. Only allowed value is 1.0 i.e. for step taken
-            counter.setText("Step Detector Detected : " + value);
+        if (activityRunning) {
+            counter.setText(String.valueOf(event.values[0]));
         }
     }
     protected void onStop() {
