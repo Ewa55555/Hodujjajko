2e8fef2855caf099bd3732216b1ca25e2b4557ea
Ewa55555 <ecia.jedi@gmail.com>
2017-05-22 17:55:45 +0200

    timer appearance

--- app/src/main/java/com/example/hodujjajko/SettingTimer.java
+++ app/src/main/java/com/example/hodujjajko/SettingTimer.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,6 +17,7 @@ public class SettingTimer extends Activity implements View.OnClickListener{
 
     private EditText timer;
     private Button startTimer;
+    private Button reset;
     private List<Button> buttons;
     private static final int[] BUTTON_IDS = {
         R.id.timer1,
@@ -41,6 +43,8 @@ public class SettingTimer extends Activity implements View.OnClickListener{
         timer.setOnClickListener(this);
         startTimer = (Button)findViewById(R.id.startTimer);
         startTimer.setOnClickListener(this);
+        reset = (Button)findViewById(R.id.reset);
+        reset.setOnClickListener(this);
         buttons = new ArrayList<>(BUTTON_IDS.length);
         for(int id : BUTTON_IDS) {
             Button button = (Button) findViewById(id);
@@ -57,31 +61,39 @@ public class SettingTimer extends Activity implements View.OnClickListener{
         boolean found = false;
         Log.i("SettingTimer", "Jestemy w on click");
         if (v.getId() == R.id.startTimer){
-            Intent timerIntent = null;
-            Log.i("SettingTimer", "przed intentem");
-            timerIntent = new Intent(getApplicationContext(), RunTimer.class);
-            Log.i("SettingTimer","tworzymy intent i extras" +timer.getText());
-            timerIntent.putExtra("text", timer.getText().toString());
-            Log.i("SettingTimer","zaraz bedziemy wlaczac runTimer");
-            startActivity(timerIntent);
-
-        }else{
-            while(i<BUTTON_IDS.length && !found) {
-                if (BUTTON_IDS[i] == v.getId()) {
-                    found = true;
-                } else {
-                    i++;
-                }
+            if(timer.getText().toString().isEmpty()){
+                Toast.makeText(this, getString(R.string.empty_timer_string), Toast.LENGTH_LONG).show();
+            }else {
+                Intent timerIntent = null;
+                Log.i("SettingTimer", "przed intentem");
+                timerIntent = new Intent(getApplicationContext(), RunTimer.class);
+                Log.i("SettingTimer", "tworzymy intent i extras" + timer.getText());
+                timerIntent.putExtra("text", timer.getText().toString());
+                Log.i("SettingTimer", "zaraz bedziemy wlaczac runTimer");
+                startActivity(timerIntent);
             }
-            if(i < BUTTON_IDS.length) {
-                String text = timer.getText().toString();
-                if (text.matches("")) {
-                    timer.setText(buttons.get(i).getText());
-                } else {
-                    if (text.charAt(text.length() - 1) == '+') {
-                        timer.setText(text + buttons.get(i).getText());
+
+        }else {
+            if (v.getId() == R.id.reset) {
+                timer.setText("");
+            } else {
+                while (i < BUTTON_IDS.length && !found) {
+                    if (BUTTON_IDS[i] == v.getId()) {
+                        found = true;
                     } else {
-                        timer.setText(text + '+' + buttons.get(i).getText());
+                        i++;
+                    }
+                }
+                if (i < BUTTON_IDS.length) {
+                    String text = timer.getText().toString();
+                    if (text.matches("")) {
+                        timer.setText(buttons.get(i).getText());
+                    } else {
+                        if (text.charAt(text.length() - 1) == '+') {
+                            timer.setText(text + buttons.get(i).getText());
+                        } else {
+                            timer.setText(text + '+' + buttons.get(i).getText());
+                        }
                     }
                 }
             }
